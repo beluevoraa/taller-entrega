1ebadd80924eaeaf16647a8f0093f469ea4be213
belén <belusolocosasimportantes@gmail.com>
2024-08-11 23:19:42 -0300

    añadí stopPropagation para evitar que se propague el evento del div al botón

--- eventos.js
+++ eventos.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const divContainer = document.getElementById('container');
+    const button = document.getElementById('button')
     
     divContainer.addEventListener('click', () => {
         alert('Hola! Soy el div');
+    });
+    button.addEventListener('click', (event) => {
+        event.stopPropagation();
+        alert('Hola!');
     });
 });
