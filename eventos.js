8f07b91f9c1559a71f445f49784d507f56567a0a
belén <belusolocosasimportantes@gmail.com>
2024-08-11 21:48:38 -0300

    agregué el código JS y le di estilos al div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const divContainer = document.getElementById('container');
+    
+    divContainer.addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
